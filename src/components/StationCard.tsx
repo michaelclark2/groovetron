979e01d287e5fe068334bdca300b21fbdfd043d5
Michael Clark <mc2.dev@proton.me>
2024-08-13 12:14:53 -0500

    added StationTags, shows bitrate and codec, country flag and tooltip, and station tags

--- src/components/StationCard.tsx
+++ src/components/StationCard.tsx
@@ -2,7 +2,52 @@ import { Station } from "radio-browser-api";
 import StarIcon from "./icons/StarIcon";
 import HomeIcon from "./icons/HomeIcon";
 import StationCardImage from "./StationCardImage";
-type TempStation = Station & { [key: string]: string };
+import { useState } from "react";
+
+function StationTags({ station }: { station: Station }) {
+  const Bitrate = ({ station }: { station: Station }) => {
+    if (station?.bitrate == 0) return null;
+    return (
+      <div className="bg-pink-300 p-1 rounded-md text-xs">
+        {station.bitrate} Kb/s {station.codec}
+      </div>
+    );
+  };
+  const CountryTag = ({ station }: { station: Station }) => {
+    const [showTitle, setShowTitle] = useState(false);
+    const getFlagEmoji = (countryCode: string) => {
+      const codePoints = countryCode
+        .toUpperCase()
+        .split("")
+        .map((char) => 127397 + char.charCodeAt());
+      return String.fromCodePoint(...codePoints);
+    };
+    return (
+      <div
+        className="bg-blue-200 p-1 rounded-md text-xs"
+        onClick={() => setShowTitle(!showTitle)}
+        onMouseOver={() => setShowTitle(true)}
+        onMouseLeave={() => setShowTitle(false)}
+      >
+        {getFlagEmoji(station.countryCode)}
+        {showTitle ? (
+          <span className="absolute block p-1 mt-2 rounded-md bg-blue-200">
+            {station.country}
+          </span>
+        ) : null}
+      </div>
+    );
+  };
+  return (
+    <div className="flex flex-wrap gap-1">
+      <Bitrate station={station} />
+      <CountryTag station={station} />
+      {station?.tags.map((tag) => (
+        <div className="bg-green-300 p-1 rounded-md text-xs">{tag}</div>
+      ))}
+    </div>
+  );
+}
 
 export default function StationCard({
   station,
@@ -15,34 +60,27 @@ export default function StationCard({
 }) {
   const isStationNowPlaying = station.id === nowPlaying?.id;
   const activeClasses = "ring ring-blue-500";
-  const tags = ["bitrate", "codec", "votes", "clickCount"];
 
   return (
     <div
       className={
-        "flex p-2 rounded-xl bg-slate-200 my-2 " +
+        "flex p-2 rounded-xl bg-slate-200 my-4 " +
         (isStationNowPlaying ? activeClasses : "")
       }
     >
       <button
         onClick={() => setNowPlaying(station)}
-        className="w-1/6 min-w-12 sm:w-1/12 flex items-center justify-center"
+        className="w-3/12 min-w-12 sm:w-1/12 flex items-center justify-center"
       >
         <StationCardImage station={station} />
       </button>
-      <div className="w-5/6 flex flex-col ml-2">
+      <div className="w-5/6 flex flex-col justify-between ml-2">
         <h3 className="font-bold overflow-hidden text-nowrap text-ellipsis">
           {station.name}
         </h3>
-        <div className="flex stationTagSection">
-          {tags.map((tag) => (
-            <div className="mr-1 bg-green-300 p-1 rounded-md text-xs">
-              {(station as TempStation)[tag]}
-            </div>
-          ))}
-        </div>
+        <StationTags station={station} />
       </div>
-      <div className="w-1/12 flex flex-col items-end">
+      <div className="w-1/12 flex flex-col items-end justify-between">
         <button className="">
           <StarIcon />
         </button>
