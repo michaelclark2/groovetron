39df3b677aeadf1e0bde1a6407495394811259ad
Michael Clark <mc2.dev@proton.me>
2024-08-13 00:55:52 -0500

    added nav sections to StationBrowser

--- src/components/StationBrowser.tsx
+++ src/components/StationBrowser.tsx
@@ -22,7 +22,12 @@ export default function StationBrowser({
 
   return (
     <section>
-      <h3 className="text-2xl">Stations</h3>
+      <h3 className="text-2xl mb-2">Stations</h3>
+      <div className="flex justify-around mb-2">
+        <div className="bg-slate-200 rounded-full p-2 px-4">Browse</div>
+        <div className="bg-slate-200 rounded-full p-2 px-4">Favorites</div>
+        <div className="bg-slate-200 rounded-full p-2 px-4">Songs</div>
+      </div>
       <div>
         {stations?.map((station) => (
           <StationCard
