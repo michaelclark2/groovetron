af633a35fae5107cda5c42229596d5179fc0d8d1
Michael Clark <mc2.dev@proton.me>
2024-08-14 10:16:21 -0500

    added loading state to StationBrowser

--- src/components/StationBrowser.tsx
+++ src/components/StationBrowser.tsx
@@ -1,8 +1,8 @@
 import { Station } from "radio-browser-api";
 import { useRadioBrowser } from "../context/RadioBrowserContext";
 import { useEffect, useState } from "react";
-import StationCard from "./StationCard";
 import StationsPagination from "./StationsPagination";
+import { RingLoader } from "react-spinners";
 
 export default function StationBrowser({
   nowPlaying,
@@ -12,15 +12,18 @@ export default function StationBrowser({
   setNowPlaying: Function;
 }) {
   const RadioBrowser = useRadioBrowser();
+  const [isLoading, setIsLoading] = useState(true);
   const [recentClicks, setRecentClicks] = useState<Station[]>([]);
   const [topVotes, setTopVotes] = useState<Station[]>([]);
 
   useEffect(() => {
     const getStations = async () => {
-      const recentClicks = await RadioBrowser.getStationsByRecentClicks(30);
-      const topVotes = await RadioBrowser.getStationsByVotes(30);
+      setIsLoading(true);
+      const recentClicks = await RadioBrowser.getStationsByRecentClicks(20);
+      const topVotes = await RadioBrowser.getStationsByVotes(20);
       setRecentClicks(recentClicks);
       setTopVotes(topVotes);
+      setIsLoading(false);
     };
     getStations();
   }, []);
@@ -32,20 +35,26 @@ export default function StationBrowser({
         <input type="text" placeholder="Search" />
       </div>
       <div className="flex flex-col gap-8">
-        <StationsPagination
-          stations={recentClicks}
-          nowPlaying={nowPlaying}
-          setNowPlaying={setNowPlaying}
-          title="Recent Clicks"
-          limit={5}
-        />
-        <StationsPagination
-          stations={topVotes}
-          nowPlaying={nowPlaying}
-          setNowPlaying={setNowPlaying}
-          title="Top Votes"
-          limit={5}
-        />
+        {isLoading ? (
+          <RingLoader className="self-center" size={200} />
+        ) : (
+          <>
+            <StationsPagination
+              stations={recentClicks}
+              nowPlaying={nowPlaying}
+              setNowPlaying={setNowPlaying}
+              title="Recent Clicks"
+              limit={5}
+            />
+            <StationsPagination
+              stations={topVotes}
+              nowPlaying={nowPlaying}
+              setNowPlaying={setNowPlaying}
+              title="Top Votes"
+              limit={5}
+            />
+          </>
+        )}
       </div>
     </div>
   );
