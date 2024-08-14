5b445a669855076a708caea8133041e058cc3e36
Michael Clark <mc2.dev@proton.me>
2024-08-14 11:18:57 -0500

    added controlled search input

--- src/components/StationBrowser.tsx
+++ src/components/StationBrowser.tsx
@@ -1,8 +1,9 @@
 import { Station } from "radio-browser-api";
 import { useRadioBrowser } from "../context/RadioBrowserContext";
-import { useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import StationsPagination from "./StationsPagination";
 import { RingLoader } from "react-spinners";
+import { IconFilterCog, IconSearch } from "@tabler/icons-react";
 
 export default function StationBrowser({
   nowPlaying,
@@ -15,6 +16,7 @@ export default function StationBrowser({
   const [isLoading, setIsLoading] = useState(true);
   const [recentClicks, setRecentClicks] = useState<Station[]>([]);
   const [topVotes, setTopVotes] = useState<Station[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const getStations = async () => {
@@ -28,11 +30,35 @@ export default function StationBrowser({
     getStations();
   }, []);
 
+  const handleSearchChange = (e: ChangeEvent) => {
+    const { value } = e.target as HTMLInputElement;
+    setSearchTerm(value);
+  };
+
+  const handleFormSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="flex flex-col gap-6">
-      <h3 className="text-3xl">Find Stations</h3>
-      <div className="w-full flex">
-        <input type="text" placeholder="Search" />
+    <div className="flex flex-col gap-6 bg-slate-200 rounded-xl p-2">
+      <h3 className="text-3xl p-2">Find Stations</h3>
+      <div className="w-full flex gap-2">
+        <form
+          className="w-full flex bg-white rounded-full p-2 gap-2"
+          onSubmit={handleFormSubmit}
+        >
+          <IconSearch />
+          <input
+            type="text"
+            placeholder="Search"
+            className="focus-visible:outline-none w-full"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </form>
+        <button className="p-2 bg-slate-100 rounded-full">
+          <IconFilterCog />
+        </button>
       </div>
       <div className="flex flex-col gap-8">
         {isLoading ? (
