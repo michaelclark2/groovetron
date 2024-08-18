f3a0b37e184baede170e4bf1aa60b35520923e7d
Michael Clark <mc2.dev@proton.me>
2024-08-18 10:53:46 -0500

    add try catch to metadata edge function

--- netlify/edge-functions/metadata.ts
+++ netlify/edge-functions/metadata.ts
@@ -3,42 +3,47 @@ import { Context } from "@netlify/edge-functions";
 export default async (req: Request, context: Context) => {
   const body = await req.json();
   const { streamUrl } = body;
-  const streamRes = await fetch(streamUrl, {
-    headers: {
-      "Icy-MetaData": "1",
-    },
-  });
-  const metaInt = streamRes.headers.get("icy-metaint", 0);
-  if (metaInt) {
-    const reader = streamRes.body!.getReader();
-    let byteCounter = 0;
-    let metadataLength = 0;
+  try {
+    const streamRes = await fetch(streamUrl, {
+      headers: {
+        "Icy-MetaData": "1",
+      },
+    });
+    const metaInt = streamRes.headers.get("icy-metaint", 0);
+    if (metaInt) {
+      const reader = streamRes.body!.getReader();
+      let byteCounter = 0;
+      let metadataLength = 0;
 
-    while (true) {
-      const { done, value } = await reader.read();
-      if (done) break;
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
 
-      for (let index = 0; index < value.length; index++) {
-        if (byteCounter === parseInt(metaInt)) {
-          // metadata block starts here
-          metadataLength = value[index] * 16;
-          // reset bytecounter
-          byteCounter = 0;
-          if (metadataLength > 0) {
-            const metadata = new TextDecoder().decode(
-              value.slice(index + 1, index + 1 + metadataLength)
-            );
-            console.log(metadata);
-            index += metadataLength;
-            return new Response(metadata);
+        for (let index = 0; index < value.length; index++) {
+          if (byteCounter === parseInt(metaInt)) {
+            // metadata block starts here
+            metadataLength = value[index] * 16;
+            // reset bytecounter
+            byteCounter = 0;
+            if (metadataLength > 0) {
+              const metadata = new TextDecoder().decode(
+                value.slice(index + 1, index + 1 + metadataLength)
+              );
+              console.log(metadata);
+              index += metadataLength;
+              return new Response(metadata);
+            }
+          } else {
+            byteCounter++;
           }
-        } else {
-          byteCounter++;
         }
       }
     }
+    return context.next(new Request(req, { body: JSON.stringify(body) }));
+  } catch (error) {
+    console.error(error);
+    return new Response("");
   }
-  return context.next(new Request(req, { body: JSON.stringify(body) }));
 };
 
 export const config = { path: "/metadata" };
